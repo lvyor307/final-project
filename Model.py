ba10387fc2014156f27d28c76f8b5581a7a061c7
orlCurve <108327560+orlCurve@users.noreply.github.com>
2024-02-14 18:29:19 +0200

    add model script to develop a structure for all the models

---
+++ Model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+
+class AudioLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(AudioLSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # Initialize hidden state with zeros
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        # Initialize cell state with zeros
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+
+        # Forward propagate LSTM
+        out, _ = self.lstm(x, (h0, c0))  # shape = (batch_size, seq_length, hidden_size)
+
+        # Decode the hidden state of the last time step
+        out = self.fc(out[:, -1, :])
+        out = self.relu(out)
+        return out
